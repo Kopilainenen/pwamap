9ae1cffa3ffc0d56a58e16db7828736b57001a4c
Shuman Denis <romlekus666@gmail.com>
2021-05-09 14:34:34 +0400

    pwamap

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.263179212756!2d37.61860631597494!3d55.753930280553696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a5a738fa419%3A0x7c347d506b52311f!2sRed%20Square!5e0!3m2!1sen!2sru!4v1620549198079!5m2!1sen!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  </div>
+</template>
+
+
+<style>
+#app iframe{
+  width: 100%;
+  height: 100vh;
+}
+
+</style>
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <scipt>
+
+  </scipt>
+</template>
